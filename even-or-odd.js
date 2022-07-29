db570e59b8ba13753b5b89241c540bc78105db09
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-07-29 21:09:44 +0100

    Check if number is even or odd

--- even-or-odd.js
+++ even-or-odd.js
@@ -1,6 +1,6 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function even_or_odd(number) {
-    //If number is divisible for 2, return "Even", else, return "Odd"
+    //If number is divisible for 2, return "Even". Else, return "Odd"
     return number%2 === 0 ? "Even" : "Odd"
 }
