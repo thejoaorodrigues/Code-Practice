0abfb2bf8e9d8b1dc6e7567a98e3fb8ec5062c80
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-07-29 21:06:27 +0100

    Multiply two numbers and return the result

--- multiply.js
+++ multiply.js
@@ -1,7 +1,7 @@
 //This code does not execute properly. Try to figure out why.
 
 function multiply(a, b){
-    //return the value of multiplication
+    //Return the value of multiplication
     return a * b
 }
   
