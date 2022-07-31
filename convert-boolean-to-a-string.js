b1aaf9d540a14e913b6c8e63c60c4cfb6d933135
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-07-31 14:15:15 +0100

    Convert a boolean value into string of 'true' or 'false'

---
+++ convert-boolean-to-a-string.js
@@ -0,0 +1,11 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+function booleanToString(b){
+    //your code here
+    //If b is truthy, return "true". Else, return "false"
+    return b ? "true" : "false"
+}
