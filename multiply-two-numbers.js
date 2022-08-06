9f5b42b3c212ce5113e9752b8ec073c409e02566
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-06 18:27:29 +0100

    Return the multiplication of two numbers

---
+++ multiply-two-numbers.js
@@ -0,0 +1,8 @@
+//Implement a function which multiplies two numbers..
+
+// Write here your multiply-function
+//Declare function multiplty, with two parameters
+function multiply(num1, num2){
+    //Return the multiplication
+    return num1 * num2
+  }
