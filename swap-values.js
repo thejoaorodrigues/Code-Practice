64c95a89ff71a13499729a5ea941d4a3b1fb1a40
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-08 13:39:44 +0100

    Swaps the values from two different indexes

---
+++ swap-values.js
@@ -0,0 +1,11 @@
+/*I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+*/
+
+function swapValues() {
+    let args = Array.prototype.slice.call(arguments);
+    let temp = args[0][0];
+    args[0][0] = args[0][1];
+    args[0][1] = temp;
+}
