0a25cbf8f62123c49bd8e0d1abcf10f6edfb9b64
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-08 16:44:28 +0100

    Removes the first and last character from an array

---
+++ remove-first-and-last-character.js
@@ -0,0 +1,35 @@
+/*
+This is a spin off of my first kata.
+
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+*/
+
+function array(arr){
+    //Good luck
+    arr = arr.split(',')
+    //If array has two elements
+    if(arr.length <= 2){
+        //Return null
+        return null
+    } else {
+        //Declare variables to remove the first and last elements
+        let first = arr.shift()
+        let last = arr.pop()
+        //Return the array joined as string
+        return arr.join(' ')
+    }
+}
+    
