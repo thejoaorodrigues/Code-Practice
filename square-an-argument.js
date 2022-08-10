8fae0f644af730f9f6f88cdf08fc6f8711df1807
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-10 15:21:26 +0100

    Returns a number, squared

---
+++ square-an-argument.js
@@ -0,0 +1,7 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+function square(num){
+    //Return num*num
+    return Math.pow(num, 2)
+}
