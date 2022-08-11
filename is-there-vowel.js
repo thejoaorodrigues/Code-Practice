9b296b5cbb7042318969c25c23afdfcfa87b2066
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-11 11:15:23 +0100

    Checks if a integer in an array is the same as the char code of a vowel

---
+++ is-there-vowel.js
@@ -0,0 +1,30 @@
+/*
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array.
+*/
+
+function isVow(a){
+    //Return a new array
+    return a.map(element =>{
+        //That, for every element, check if char code of element is equal to a vowel
+        if (String.fromCharCode(element) === 'a'){
+            //And returns the said vowel
+            return 'a';
+        } else if (String.fromCharCode(element) === 'e'){
+            return 'e';
+        } else if (String.fromCharCode(element) === 'i'){
+            return 'i';
+        } else if (String.fromCharCode(element) === 'o'){
+            return 'o';
+        } else if (String.fromCharCode(element) === 'u'){
+            return 'u';
+        //Else
+        } else {
+            //Returns the element
+            return element
+        }
+    })
+}
