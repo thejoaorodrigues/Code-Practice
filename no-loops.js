58a939b29a1c7c67befa4e7df177eb5b04fce55e
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-11 10:50:56 +0100

    Check is an aray contains x element

---
+++ no-loops.js
@@ -0,0 +1,12 @@
+/*
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+*/
+
+function check(a,x){
+    //Check if array a includes x
+    return a.includes(x) ? true : false
+};
