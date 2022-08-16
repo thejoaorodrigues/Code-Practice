c009522301699b1ac19151e818d26fa373441c45
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-16 18:44:49 +0100

    Converts bin number into decimal

---
+++ bin-to-decimal.js
@@ -0,0 +1,7 @@
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+    // ...
+    //Parseint the bin value into decimal value
+    return parseInt(bin, 2)
+  }
