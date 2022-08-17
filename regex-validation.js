4a04b4b485d129d6770c82d85883ec79a477a8df
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-17 20:45:43 +0100

    Validation with regex expressions

---
+++ regex-validation.js
@@ -0,0 +1,14 @@
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+*/
+
+function validateUsr(username) {
+    //Regex validate number
+    res = /^[0-9a-z_]{4,16}$/.test(username)
+    return res
+  }
