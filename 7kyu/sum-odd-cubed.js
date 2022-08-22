a659e9ffa5cdf605a64e53466a073bc93a07556b
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-22 19:15:51 +0100

    Returns the sum of the odd numbers of an array, cubed

--- 7kyu/sum-odd-cubed.js
+++ 7kyu/sum-odd-cubed.js
@@ -2,3 +2,8 @@
 Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 */
 
+function cubeOdd(arr) {
+    arr = arr.filter(element => element%2 != 0);
+    arr = arr.reduce((acc, val) => acc + Math.pow(val, 3), 0);
+    return isNaN(arr) ? undefined : arr;
+  }
