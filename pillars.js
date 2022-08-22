dc09f82d98b0ae5926e1f38d6731afd2e04362ed
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-22 21:14:05 +0100

    Calculates distance between pillars

---
+++ pillars.js
@@ -0,0 +1,17 @@
+/*
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+*/
+
+function pillars(numPill, dist, width) {
+    // your code here
+    if(numPill === 1){
+      return 0
+    } else {
+      return dist*100*(numPill-1) + width*numPill - 2*width
+    }
+  }
