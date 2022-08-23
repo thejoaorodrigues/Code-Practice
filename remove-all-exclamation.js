4a19df175e9b0d01c4fe11171f3ea7ce7d50db2b
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-23 21:53:47 +0100

    Removes the exclamation mark from the last word of a string

---
+++ remove-all-exclamation.js
@@ -0,0 +1,16 @@
+/*
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+function remove(s){
+    //Replace string using Regex
+    return s.replace(/!+$/, '');
+  }
