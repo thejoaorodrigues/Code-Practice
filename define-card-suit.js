a1f384df4b1d4514af383a43aa1cf098bcfefd6d
Joao Rodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-23 21:46:16 +0100

    Returns the suit of a given card

---
+++ define-card-suit.js
@@ -0,0 +1,24 @@
+/*
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+function defineSuit(card) {
+    // good luck
+    //Check condition for suit of card
+    if(card[1] === '♣'){
+      return 'clubs'
+    } else if (card[1] === '♠'){
+      return 'spades'
+    } else if (card[1] === '♦'){
+      return 'diamonds'
+    } else if (card[1] === '♥'){
+      return 'hearts'
+    }
+  }
