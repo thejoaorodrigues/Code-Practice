3dd359e88b09f20d4c23e068375d82c4d008d497
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-29 22:47:47 +0100

    Returns the largest elements of an array

--- 7kyu/largest-elements.js
+++ 7kyu/largest-elements.js
@@ -7,3 +7,9 @@ largest(2, [7,6,5,4,3,2,1])
 // => [6,7]
 */
 
+function largest(n,xs){
+    // Find the n highest elements in a list
+    //Sort the array
+  xs.sort((a, b) => a - b)
+  return n === 0 ? [] : xs.slice(-n)
+}
