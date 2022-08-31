fbdad66255dab06f9a3969f9471deb7c24891292
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-08-31 22:54:21 +0100

    Filters the unused numbers of numbers passed

---
+++ 7kyu/filter-unused-digits.js
@@ -0,0 +1,25 @@
+/*
+Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+Example:
+
+[12, 34, 56, 78]  =>  "09"
+[2015, 8, 26]     =>  "3479"
+Note: the digits in the resulting string should be sorted.
+*/
+
+function unusedDigits(...args) {
+    // Given: varying # of integer arguments
+    // define the parameter
+    // create a variable   
+    
+    const numbers = [0,1,2,3,4,5,6,7,8,9];
+    
+    // change the arguments from integer into a string   
+    //join 
+    let numStr = args.join('');  
+    
+    // filter out those that are not present in any of the arguments
+    // Return: a sorted string of digits
+    return numbers.filter(element => !numStr.includes(element)).join('')
+  }
