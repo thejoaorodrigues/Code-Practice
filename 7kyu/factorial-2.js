c5e5a91ab9d9d1edb84c3c3649994fb8ae5f7aef
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-02 00:00:52 +0100

    Retrurns the factorial of a number

---
+++ 7kyu/factorial-2.js
@@ -0,0 +1,10 @@
+//Your task is to write function factorial.
+
+function factorial(n){
+    //your code here
+    let result = 1
+    for(i=1; i<=n; i++){
+      result *= i
+    }
+    return result
+  }
