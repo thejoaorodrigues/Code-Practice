f9d40691863b03fa72209cba11ec77336ab09154
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-12 14:41:52 +0100

    Digitize numbers into array

---
+++ 7kyu/digitize.js
@@ -0,0 +1,21 @@
+/*
+Given a non-negative integer, return an array / a list of the individual digits in order.
+
+Examples:
+
+123 => [1,2,3]
+
+1 => [1]
+
+8675309 => [8,6,7,5,3,0,9]
+
+
+*/
+
+function digitize(n) {
+    //Return the new array
+    return n.toString().split('').map(element => {
+        //With the parsed elements
+      return +element
+    })
+  }
