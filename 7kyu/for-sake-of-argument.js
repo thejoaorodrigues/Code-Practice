8930db3f4520811685521e1625f415634855624c
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-13 18:03:07 +0100

    Checks if all elements in an array are numbers

---
+++ 7kyu/for-sake-of-argument.js
@@ -0,0 +1,16 @@
+/*
+Write a function named numbers.
+
+function should return True if all parameters are of the Number type.
+
+The function should accept any number of parameters.
+
+Example usage:
+
+numbers(1, 4, 3, 2, 5); // true
+numbers(1, "a", 3); // false
+numbers(1, 3, NaN); // true
+*/
+
+// Create your numbers function here
+const numbers = (...arr) => arr.every(element => typeof element === 'number')
