4cd9a5a434ee9d18336c0d29f927b17fc713f5d3
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-19 21:50:20 +0100

    Checks if a given number is prime

--- 6kyu/is-a-number-prime.js
+++ 6kyu/is-a-number-prime.js
@@ -13,3 +13,19 @@ Example
 // is_prime(-1) /* false */
 /**/
 
+function isPrime(num) {
+    if (num < 2) {
+       return false;
+    }
+  if (num === 2) {
+       return true;
+    }
+    
+    const maximumDividor = Math.sqrt(num) + 1;
+  for (let i = 2; i < maximumDividor; i++) {
+       if (num % i === 0) {
+         return false;
+       }
+    }
+  return true;
+  }
