0afac67c2033448e61b6c8dc1e5b55ad704045b9
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-21 23:03:53 +0100

    builds an ascii tower

--- 6kyu/build-tower.js
+++ 6kyu/build-tower.js
@@ -22,3 +22,18 @@ And a tower with 6 floors looks like this:
 Go challenge Build Tower Advanced once you have finished this :)
 */
 
+function towerBuilder(nFloors) {
+    // build here
+    let base = nFloors*2-1
+    let hash = '*'
+    let space = ' '
+    let output = []
+    for(i=1; i<=nFloors; i++){
+      let current = i*2-1
+      let spaceCounter = base-current
+      console.log(current, spaceCounter)
+      let string = `${space.repeat(spaceCounter/2)}${hash.repeat(current)}${space.repeat(spaceCounter/2)}`
+      output.push(string)
+    }
+    return output
+  }
