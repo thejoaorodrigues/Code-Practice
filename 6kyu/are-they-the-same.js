2ae6cdd55b2197f321a26120e0e5c23bb2c0542d
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-23 20:44:26 +0100

    Checks if the element of an array is the square root of another element of an array

--- 6kyu/are-they-the-same.js
+++ 6kyu/are-they-the-same.js
@@ -29,25 +29,9 @@ Note for C
 The two arrays have the same size (> 0) given as parameter in function comp.
 */
 
-function comp(array1, array2){
-    let result = false;
-   
-    if ( array1 && array2 ) {
-      if (array1.length === 0 && array2.length === 0){
-        result = true;
-      } else {
-        const squareRoots = array2.map(num => Math.sqrt(num));
-        array1.sort((a, b) => a-b);
-        squareRoots.sort((a, b) => a-b);
-        for (let i=0; i<squareRoots.length; i++) {
-          if (array1.indexOf(squareRoots[i]) 
-              !== array1.indexOf(array1[i])) {
-            result = false;
-            break;
-          }
-          result = true;
-        }
-      }
-    }
-    return result;
+function comp(array1, array2) {
+    if (array1 == null || array2 == null) return false;
+    array1.sort((a, b) => a - b);
+    array2.sort((a, b) => a - b);
+    return array1.every((item, index) => item * item == array2[index]);
   }
