d4cb71fc4ea1d4bf3175285683c37ad9e0fd2b5c
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-09-23 21:08:22 +0100

    Counts the number of characters in a string

--- 6kyu/count-chars-in-string.js
+++ 6kyu/count-chars-in-string.js
@@ -4,3 +4,9 @@ The main idea is to count all the occurring characters in a string. If you have 
 What if the string is empty? Then the result should be empty object literal, {}.
 */
 
+function count (string) {  
+    // The function code should be here
+    let output={};
+    string.split('').map(element => output[element] = output[element]+1 || 1)
+    return output;
+  }
