6139afd1634c94ce3741aa3b7ef7c9b6df60dd14
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-11-01 13:35:28 +0000

    Returns the True counts of a list

---
+++ python/8ky/counting-sheep.py
@@ -0,0 +1,26 @@
+'''
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+'''
+
+def count_sheeps(sheep):
+    #Todo May the force be with you
+    sum = 0
+    for value in sheep:
+        if value:
+            sum += 1
+    return sum
+
+def count_sheeps(arrayOfSheeps):
+    return arrayOfSheeps.count(True)
