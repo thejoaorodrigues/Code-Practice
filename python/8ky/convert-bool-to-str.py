7304278e4b0de514dd03c254274086d0a4b24a47
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-11-02 20:05:10 +0000

    converts a boolean value to a string representation

---
+++ python/8ky/convert-bool-to-str.py
@@ -0,0 +1,9 @@
+'''
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+'''
+
+def boolean_to_string(b):
+    #your code here
+    return 'True' if b else 'False'
