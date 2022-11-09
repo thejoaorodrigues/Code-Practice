95c11c8ac2d2707c357f2ce6cc70e743bc2ca20d
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-11-09 20:42:52 +0000

    Reverses a list of words

---
+++ python/8ky/reversed-words.py
@@ -0,0 +1,10 @@
+'''
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+'''
+
+def reverse_words(s):
+    return ' '.join(s.split(' ')[::-1])
