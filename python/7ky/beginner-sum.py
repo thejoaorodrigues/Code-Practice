99187f9dd9443b17837d772e3188047a7bf4967f
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-11-16 22:38:28 +0000

    Sums a range object from a to b

---
+++ python/7ky/beginner-sum.py
@@ -0,0 +1,20 @@
+'''
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+'''
+
+def get_sum(a,b):
+    #good luck!
+    return sum(range(a, b+1)) if b > a else sum(range(b, a+1)) 
+
+def get_sum(a,b):
+    return sum(range(min(a, b), max(a, b) + 1))
