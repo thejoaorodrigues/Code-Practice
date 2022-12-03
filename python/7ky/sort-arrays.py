adbaddeb96a93ad326eb38fdd6b2ee1fca04c68c
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-12-03 15:28:14 +0000

    Sort array

---
+++ python/7ky/sort-arrays.py
@@ -0,0 +1,9 @@
+'''
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+'''
+
+# input: names - unsorted list
+# output: sorted list
+def sortme(names):
+    #your code here
+    return sorted(names)
