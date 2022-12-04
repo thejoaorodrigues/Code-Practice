3b52b0511189fe9ae1461b466962ea2dca58c2a7
thejoaorodrigues <joaomanuelribeirorodrigues@gmail.com>
2022-12-04 22:31:14 +0000

    Sums index of letters in words

---
+++ python/7ky/love-vs-frienship.py
@@ -0,0 +1,21 @@
+'''
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+'''
+
+def words_to_marks(s):
+    # Easy one
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    output = 0
+    for i in range(len(s)):
+        output += alphabet.index(s[i])+1
+    return output
