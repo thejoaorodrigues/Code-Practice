450788ff8f2f320839de674f90aba117bd53c73d
unknown <joaomanuelribeirorodrigues@gmail.com>
2023-07-31 21:53:32 +0100

    Returns the sum of the positive numbers in an array

---
+++ code-wars/python/8kyu/sum_of_positive.py
@@ -0,0 +1,16 @@
+'''
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+'''
+
+def positive_sum(arr):
+    # Your code here
+    output = 0
+    for item in arr:
+        if item > 0:
+            output += item
+    return output
+    # return sum(x for x in arr if x > 0)
